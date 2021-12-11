cfe4a36c38914f3bc3361d6ece389d81e72aa542
Muhammad Fadhel <77598231+tRucedhel@users.noreply.github.com>
2021-12-11 12:56:41 +0800

    Add files via upload

---
+++ DownloadYTtoMP3.py
@@ -0,0 +1,19 @@
+from urllib.request import CacheFTPHandler
+from pytube import YouTube
+
+while(True) :
+    url = input("Masukkan Link :")
+    my_video = YouTube(url)
+    print("Apakah judul video yang didownload :")
+    print(my_video.title)
+
+    jawab = input("Y/N :")
+    if(jawab.lower() =="y") :
+        break
+
+
+my_video = my_video.streams.get_audio_only()
+my_video.download()
+
+
+
